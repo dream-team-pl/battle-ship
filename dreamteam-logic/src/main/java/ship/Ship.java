00a6e37425b2a46354b64f148a39d786e210fd4c
Daniel Zaucha <Daniel_Zaucha@epam.com>
2016-07-11 21:50:12 +0200

    First class was created in TDD manner. Comments was added, and DataProviders are used.

--- dreamteam-logic/src/main/java/ship/Ship.java
+++ dreamteam-logic/src/main/java/ship/Ship.java
@@ -2,7 +2,41 @@ package ship;
 
 /**
  * Created by daniel on 11.07.16.
+ * Simple representation of our ships. Variables are protected to get access
+ * to test part in project. Ship could have size between 1 and 4. Name have to
+ * be longer than 0.
  */
 public class Ship {
 
+    final protected String name;
+    final protected byte size;
+    protected int damaged;
+
+    /**
+     *
+     * @param name - name of ship
+     * @param size - size of ship
+     */
+    public Ship(String name, byte size) {
+        if(size <= 0 || size >4) throw new IllegalArgumentException();
+        if(name.length() == 0) throw new IllegalArgumentException();
+
+        this.name = name;
+        this.size = size;
+    }
+
+    /**
+     * decrement damaged variable. We need it when we check if the ship is destroyed.
+     */
+    public void damage() {
+        damaged++;
+    }
+
+    /**
+     *
+     * @return true if the ship is destroyed and false if ship alived
+     */
+    public boolean isDamaged() {
+        return (size - damaged) == 0;
+    }
 }
--- dreamteam-logic/src/test/ship/ShipTest.java
+++ dreamteam-logic/src/test/ship/ShipTest.java
@@ -1,16 +1,162 @@
 package ship;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 /**
  * Created by daniel on 11.07.16.
+ * Test of Ship class.
  */
 public class ShipTest {
 
-    @Test
-    public void firstTest() {
-        assertTrue(true);
+    @DataProvider
+    public Object[][] names (){
+        return new Object[][] {
+                {"ship1", },
+                {"ship2"},
+                {"destroyer"},
+                {"Al-Pacino"},
+                {"Titanic"},
+                {""}
+        };
+    }
+
+    /**
+     *
+     * @param name - is delivered to check if after creating object Ship, name is initialized
+     */
+    @Test(dataProvider = "names", expectedExceptions = IllegalArgumentException.class)
+    public void setName(String name) {
+        // given
+        // hardcoding size of ship. It is irrelevant
+        Ship ship = new Ship(name, (byte) 0);
+
+        // when
+        // ...
+
+        // then
+        assertEquals(ship.name, name);
+    }
+
+    @DataProvider
+    public Object[][] sizes (){
+        return new Object[][] {
+                {(byte) 0},
+                {(byte) 1},
+                {(byte) 2},
+                {(byte) 3},
+                {(byte) 4},
+                {(byte) 0},
+                {(byte) 7}
+        };
+    }
+
+    /**
+     *
+     * @param size - is delivered to check if after creating object Ship, size is initialized
+     */
+    @Test(dataProvider = "sizes", expectedExceptions = IllegalArgumentException.class)
+    public void setSize(byte size) {
+        // given
+        // hardcoding size of ship. It is irrelevant
+        Ship ship = new Ship("", size);
+
+        // when
+        // ...
+
+        // then
+        assertEquals(ship.size, size);
+    }
+
+    @DataProvider
+    public Object[][] damaging() {
+        return new Object[][] {
+                {(byte)4, 3},
+                {(byte)4, 2},
+                {(byte)4, 1},
+                {(byte)3, 3},
+                {(byte)2, 1},
+                {(byte)1, 1}
+        };
+    }
+
+    /**
+     *
+     * @param size - is initialized, to prevent from to many call of damage() method
+     * @param damaged - how many times damage() method is called
+     */
+    @Test(dataProvider = "damaging")
+    public void shipIsHit(byte size, int damaged) {
+        // given
+        Ship ship = new Ship("ship", size);
+
+        // when
+        for(int i=0; i<damaged; i++)
+            ship.damage();
+
+        // then
+        assertEquals(ship.damaged, damaged);
+    }
+
+    @DataProvider
+    public Object[][] sizeAndDamagedSet() {
+        return new Object[][] {
+                {(byte) 4, 4},
+                {(byte) 3, 3},
+                {(byte) 2, 2},
+                {(byte) 1, 1}
+        };
+    }
+
+    /**
+     *
+     * @param size - is initialized, to prevent from to many call of damage() method, and check how many times
+     *             ship can be hit.
+     * @param damaged - how many times damage() method is called
+     */
+    @Test(dataProvider = "sizeAndDamagedSet")
+    public void isWinner(byte size, int damaged) {
+        // given
+        Ship ship = new Ship("ship", size);
+
+        // when
+        for(int i=0; i<damaged; i++)
+            ship.damage();
+
+        // given
+        assertTrue(ship.isDamaged());
+    }
+
+    @DataProvider
+    public Object[][] sizeAndDamagedSet2() {
+        return new Object[][] {
+                {(byte) 4, 3},
+                {(byte) 3, 2},
+                {(byte) 2, 1},
+                {(byte) 4, 2},
+                {(byte) 4, 1},
+                {(byte) 3, 1}
+        };
+    }
+
+    /**
+     *
+     * @param size - is initialized, to prevent from to many call of damage() method, and check how many times
+     *             ship can be hit.
+     * @param damaged - how many times damage() method is called
+     */
+    @Test(dataProvider = "sizeAndDamagedSet2")
+    public void isNotWinner(byte size, int damaged) {
+        // given
+        Ship ship = new Ship("ship", size);
+
+        // when
+        for(int i=0; i<damaged; i++)
+            ship.damage();
+
+        // given
+        assertFalse(ship.isDamaged());
     }
 }
