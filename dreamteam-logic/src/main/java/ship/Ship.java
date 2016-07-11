1db32c212a68c42652a7335e0a4bcc7f984ddbf7
Daniel Zaucha <Daniel_Zaucha@epam.com>
2016-07-11 20:39:30 +0200

    TestNG and Mockito added in dependency. jUnit was removed because it's better for us to use testNG. I create first classes and first test to store our src/main/java and src/test because without files, git don't store folders. In addition testing.xml was added. You can run this file to run suite of tests that you define in this xml file.

---
+++ dreamteam-logic/src/main/java/ship/Ship.java
@@ -0,0 +1,8 @@
+package ship;
+
+/**
+ * Created by daniel on 11.07.16.
+ */
+public class Ship {
+
+}
---
+++ dreamteam-logic/src/test/ship/ShipTest.java
@@ -0,0 +1,16 @@
+package ship;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by daniel on 11.07.16.
+ */
+public class ShipTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
