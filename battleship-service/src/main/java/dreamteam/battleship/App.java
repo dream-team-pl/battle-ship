241bdc77710cf02d5905dc928fb317c822f83c6a
Ehsan Golesorkhi <ehsan_golesorkhi@epam.com>
2016-07-18 14:53:57 +0200

    battleship-service created

---
+++ battleship-service/src/main/java/dreamteam/battleship/App.java
@@ -0,0 +1,13 @@
+package dreamteam.battleship;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
