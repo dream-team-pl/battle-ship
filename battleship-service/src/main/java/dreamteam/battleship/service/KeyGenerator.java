3254dc04c10bddf0c14d2bb55d1db89e5050695e
Ehsan Golesorkhi <ehsan_golesorkhi@epam.com>
2016-07-18 16:15:24 +0200

    the registration service is implemented every session created can register seperately the player

---
+++ battleship-service/src/main/java/dreamteam/battleship/service/KeyGenerator.java
@@ -0,0 +1,18 @@
+package dreamteam.battleship.service;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+/**
+ * Created by ehsan on 18.07.16.
+ */
+public class KeyGenerator {
+
+
+    public String generate() {
+        StringBuilder builder = new StringBuilder();
+        Random rand = new Random(100);
+        builder.append(System.currentTimeMillis()).append(rand.nextFloat()).append(new Object().hashCode());
+        return builder.toString();
+    }
+}
---
+++ battleship-service/src/main/java/dreamteam/battleship/service/Player.java
@@ -0,0 +1,18 @@
+package dreamteam.battleship.service;
+
+/**
+ * Created by ehsan on 18.07.16.
+ */
+public class Player {
+
+    public final String name;
+    public final String surname;
+    public final String identification;
+
+
+    public Player(String name, String surname, String identification) {
+        this.name = name;
+        this.surname = surname;
+        this.identification = identification;
+    }
+}
