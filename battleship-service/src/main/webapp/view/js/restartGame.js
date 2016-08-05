6520c6c2a333014890655a1838a88148b9e1cb2a
Grzegorz Sledz <sledzgrzegorz@gmail.com>
2016-08-05 11:37:56 +0200

    Restart game was added(only front-end possibility without connecting to server). Also copyright footer was added

---
+++ battleship-service/src/main/webapp/view/js/restartGame.js
@@ -0,0 +1,9 @@
+function showEndOfTheGameModal(data) {
+    $("#winner_id").append(data.winner.name + " " + data.winner.surname);
+    $('#endOfTheGameModal').modal('show');
+}
+
+function restartGame() {
+    $("#winner_id").empty();
+    $('#endOfTheGameModal').modal('hide');
+}
