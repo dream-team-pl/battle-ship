f39045b67d93be6dcf7b77432c534acf5b4c52b2
Ehsan Golesorkhi <ehsan_golesorkhi@epam.com>
2016-08-22 12:05:31 +0200

    made the responses private

--- battleship-service/src/test/java/dreamteam/battleship/service/springcontroller/model/response/ModelResponseTestUtil.java
+++ battleship-service/src/test/java/dreamteam/battleship/service/springcontroller/model/response/ModelResponseTestUtil.java
@@ -3,6 +3,8 @@ package dreamteam.battleship.service.springcontroller.model.response;
 import dreamteam.battleship.logic.movement.MovementStatus;
 import dreamteam.battleship.service.springcontroller.model.Player;
 
+import java.util.Map;
+
 /**
  * Created by ehsan on 11.08.16.
  */
@@ -18,5 +20,29 @@ public class ModelResponseTestUtil {
 
     public static boolean readyToPlay(Response response){
         return ((Organizer)response).readyToPlay;
+    }
+
+    public static MovementStatus shootingStatus(Response response){
+        return ((ShootingResult)response).status;
+    }
+
+    public static int numberOfShots(Response response){
+        return ((TurnStatus)response).numberOfShots;
+    }
+
+    public static Map<Integer, Boolean> myDamages(Response response){
+        return ((TurnStatus)response).myDamages;
+    }
+
+    public static boolean isMyTurn(Response response){
+        return ((TurnStatus)response).isMyTurn;
+    }
+
+    public static Player winner(Response response){
+        return ((TurnStatus)response).winner;
+    }
+
+    public static boolean gameOver(Response response){
+        return ((TurnStatus)response).gameOver;
     }
 }
